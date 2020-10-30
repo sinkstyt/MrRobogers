5f09572c17c0b6b883b7fdc91201402591ea4ab1
Tyler Sinks <tyler.sinks@gmail.com>
2020-10-30 17:48:59 -0400

    get second spec passing

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ function mrRobogerSpeaks(input) {
   // Look for a "1" in the input, NB: typeof input //--> string
   // Use a loop to check all the characters of the string for first instance of "1"
   for (const char of input) {
+    if (char == "2") {
+      response = "Boop!";
+      return response;
+    }
     if (char == "1") {
       response = "Beep!";
       return response;
